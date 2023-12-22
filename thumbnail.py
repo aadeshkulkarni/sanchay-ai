ddd7ececdc49cdd8f956e3d32eb0b51c8eaf335e
SavviSam <swghazzawi@gmail.com>
2023-12-22 16:33:36 -0700

    added Stability API env and connected to gRPC API

---
+++ thumbnail.py
@@ -0,0 +1,53 @@
+import os
+import io
+import warnings
+from PIL import Image
+from stability_sdk import client
+import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
+import ffmpeg
+import sys
+from pprint import pprint
+
+# gRPC host for Stability AI
+STABILITY_HOST = os.environ.get("STABILITY_HOST")
+STABILITY_KEY = os.environ.get("STABILITY_KEY")
+INPUT_FILE = os.environ.get("INPUT_FILE_PATH")
+OUTPUT_PATH = os.environ.get("OUTPUT_DIR")
+
+
+# Set up our connection to the API.
+stability_api = client.StabilityInference(
+    key=STABILITY_KEY, # API Key reference.
+    verbose=True, # Print debug messages.
+    engine="stable-diffusion-xl-1024-v1-0", # Set the engine to use for generation.
+)
+
+# Set up our initial generation parameters.
+answers = stability_api.generate(
+    prompt="./videos/How I Failed My Netflix Interview.mp4",
+    seed=4253978046, # If a seed is provided, the resulting generated image will be deterministic.
+                     # What this means is that as long as all generation parameters remain the same, you can always recall the same image simply by generating it again.
+                     # Note: This isn't quite the case for Clip Guided generations, which we'll tackle in a future example notebook.
+    steps=50, # Amount of inference steps performed on image generation. Defaults to 30. 
+    cfg_scale=8.0, # Influences how strongly your generation is guided to match your prompt.
+                   # Setting this value higher increases the strength in which it tries to match your prompt.
+                   # Defaults to 7.0 if not specified.
+    width=1280, # Generation width, defaults to 512 if not included.
+    height=720, # Generation height, defaults to 512 if not included.
+    samples=1, # Number of images to generate, defaults to 1 if not included.
+    sampler=generation.SAMPLER_K_DPMPP_2M # Choose which sampler we want to denoise our generation with.
+                                                 # Defaults to k_dpmpp_2m if not specified. Clip Guidance only supports ancestral samplers.
+                                                 # (Available Samplers: ddim, plms, k_euler, k_euler_ancestral, k_heun, k_dpm_2, k_dpm_2_ancestral, k_dpmpp_2s_ancestral, k_lms, k_dpmpp_2m, k_dpmpp_sde)
+)
+
+# Set up our warning to print to the console if the adult content classifier is tripped.
+# If adult content classifier is not tripped, save generated images.
+for resp in answers:
+    for artifact in resp.artifacts:
+        if artifact.finish_reason == generation.FILTER:
+            warnings.warn(
+                "Your request activated the API's safety filters and could not be processed."
+                "Please modify the prompt and try again.")
+        if artifact.type == generation.ARTIFACT_IMAGE:
+            img = Image.open(io.BytesIO(artifact.binary))
+            file_name = img.save(f'{INPUT_FILE}.png') # Save our generated images with their seed number as the filename.
---
+++ thumbnail.sh
@@ -0,0 +1,9 @@
+export STABILITY_KEY=""
+export STABILITY_HOST="grpc.stability.ai:443"
+export INPUT_FILE_PATH="./videos/video.mp4"
+export OUTPUT_DIR="./output" # Directory where output will be stored
+
+
+# Generate video thumbnail
+python3 thumbnail.py
+
